6ecfb12bfd8955be0eea128921e7adc841dd0653
Debottam Kundu <debottam.kundu330@gmail.com>
2026-04-09 12:33:37 +0200

    fix memory leaks

--- communication_subspace/ibl_communication/collate_cross_epoch_stats.py
+++ communication_subspace/ibl_communication/collate_cross_epoch_stats.py
@@ -6,6 +6,50 @@ from one.api import ONE
 from tqdm import tqdm
 
 
+def extract_records(data_dict, source_type, animal_idx):
+    """Extracts records as a list of dicts, filtering out unwanted frames immediately."""
+    records = []
+    for region_1, nested_dict in data_dict.items():
+        for region_2, frame_dict in nested_dict.items():
+            for frame_pair, metrics in frame_dict.items():
+
+                source_frame, choice_frame = frame_pair
+                if choice_frame in [0, 1]:
+                    continue
+                # Keep ONLY source frames 0 and 1
+                if source_frame not in [0, 1]:
+                    continue
+
+                r2 = metrics.get("r2_scores")
+
+                if isinstance(r2, (list, np.ndarray)):
+                    for score in r2:
+                        records.append(
+                            {
+                                "animal_idx": animal_idx,
+                                "region_1": region_1,
+                                "region_2": region_2,
+                                "source_type": source_type,
+                                "source_frame": source_frame,
+                                "choice_frame": choice_frame,
+                                "r2_score": score,
+                            }
+                        )
+                else:
+                    records.append(
+                        {
+                            "animal_idx": animal_idx,
+                            "region_1": region_1,
+                            "region_2": region_2,
+                            "source_type": source_type,
+                            "source_frame": source_frame,
+                            "choice_frame": choice_frame,
+                            "r2_score": r2,
+                        }
+                    )
+    return records
+
+
 def flatten_prediction_dict(animal_dict, source_type, animal_id):
     """Flattens your nested dictionary into a Pandas DataFrame."""
     records = []
@@ -51,31 +95,60 @@ if __name__ == "__main__":
     sessions = [eid.split("/")[-1].split("_")[0] for eid in all_eids]
     sessions = np.unique(sessions)
 
-    df_all = []
-    for idx, eid in tqdm(enumerate(sessions)):
+    # df_all = []
+    # for idx, eid in tqdm(enumerate(sessions)):
+
+    #     filea = f"./data/crossprediction/{eid}_results_stim_choice.pkl"
+    #     fileb = f"./data/crossprediction/{eid}_results_prior_choice.pkl"
+
+    #     try:
+
+    #         data_stim = pkl.load(open(filea, "rb"))
+    #         data_prior = pkl.load(open(fileb, "rb"))
+
+    #         df_stim = flatten_prediction_dict(data_stim, "Stimulus", idx)
+    #         df_prior = flatten_prediction_dict(data_prior, "Prior", idx)
+
+    #         df_raw = pd.concat([df_prior, df_stim], ignore_index=True)
+
+    #         # do some housekeeping, throw away frames I think are overlapping
+    #         df_raw = df_raw[
+    #             (~df_raw["choice_frame"].isin([0, 1]))
+    #             & ((df_raw["source_frame"] == 0) | (df_raw["source_frame"] == 1))
+    #         ]  # keep it for now
+    #         df_all.append(df_raw)
+    #     except Exception as e:
+    #         print(f"Error with {eid}, probably not generated yet", e)
+    #         continue
+
+    # df_all = pd.concat(df_all, ignore_index=True)
+    # df_all.to_parquet("./data/crossprediction/crossprediction_results.pqt")
+
+    all_records = []
+    for idx, eid in tqdm(enumerate(sessions), total=len(sessions)):
 
         filea = f"./data/crossprediction/{eid}_results_stim_choice.pkl"
         fileb = f"./data/crossprediction/{eid}_results_prior_choice.pkl"
 
         try:
 
-            data_stim = pkl.load(open(filea, "rb"))
-            data_prior = pkl.load(open(fileb, "rb"))
+            with open(filea, "rb") as fa, open(fileb, "rb") as fb:
+                data_stim = pkl.load(fa)
+                data_prior = pkl.load(fb)
 
-            df_stim = flatten_prediction_dict(data_stim, "Stimulus", idx)
-            df_prior = flatten_prediction_dict(data_prior, "Prior", idx)
+            records_stim = extract_records(data_stim, "Stimulus", idx)
+            records_prior = extract_records(data_prior, "Prior", idx)
 
-            df_raw = pd.concat([df_prior, df_stim], ignore_index=True)
+            all_records.extend(records_stim)
+            all_records.extend(records_prior)
 
-            # do some housekeeping, throw away frames I think are overlapping
-            df_raw = df_raw[
-                (~df_raw["choice_frame"].isin([0, 1]))
-                & ((df_raw["source_frame"] == 0) | (df_raw["source_frame"] == 1))
-            ]  # keep it for now
-            df_all.append(df_raw)
         except Exception as e:
-            print(f"Error with {eid}, probably not generated yet", e)
+            print(f"Error with {eid}, probably not generated yet. Details: {e}")
             continue
 
-    df_all = pd.concat(df_all, ignore_index=True)
+    print("Converting to DataFrame...")
+    df_all = pd.DataFrame(all_records)
+
+    print("Saving to Parquet...")
     df_all.to_parquet("./data/crossprediction/crossprediction_results.pqt")
+    print("Done!")
